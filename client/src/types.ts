41945660d627e3ec032ab06dcb38aba42269d32f
unknown <suarezjav89@gmail.com>
2022-05-10 23:42:00 -0300

    add h24 base files

---
+++ client/src/types.ts
@@ -0,0 +1,35 @@
+export type Category = {
+  name: string
+  categoryArticles: CategoryArticle
+  articleCount: number
+  childrenCategories: ChildCategory
+}
+
+export type Article = {
+  name:  string
+  variantName: string
+  prices: Prices
+  images: Image[]
+}
+
+export type ChildCategory = {
+  list: Array<{
+    name: string
+    urlPath: string
+  }>
+}
+
+export type Prices = {
+  currency: string
+  regular: {
+    value: number
+  }
+}
+
+export type Image = {
+  path: string
+}
+
+export type CategoryArticle = {
+  articles: Article[]
+}
